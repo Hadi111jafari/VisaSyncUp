f1a68cd966e0fb4581225e9af929d808606ecc71
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2024-03-05 14:49:48 +0430

    Create Input component

---
+++ src/components/VisaApplication/InputDropdown.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './InputDropdown.css';
+
+const InputDropdown = ({ label, options, defaultValue, instruction }) => {
+  const [inputValue, setInputValue] = useState(defaultValue || '');
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+  const inputRef = useRef(null);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    setShowDropdown(true);
+  };
+
+  const selectOption = (value) => {
+    setInputValue(value);
+    setShowDropdown(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  let filteredOptions = [];
+
+  if (options && options.length > 0) {
+    filteredOptions = options.filter((option) => option.includes(inputValue));
+  }
+
+  const input = (
+    <>
+      <input
+        type="text"
+        className="w-full px-4 py-2 rounded-md border focus:outline-none focus:border-blue-500"
+        placeholder={label}
+        value={inputValue}
+        onClick={() => setShowDropdown(true)}
+        onChange={handleInputChange}
+        ref={inputRef}
+        dir="rtl"
+      />
+    </>
+  );
+
+  return (
+    <div className="mb-4 flex items-center w-full ">
+      <div className="flex-col">
+        <div className="w-full pr-4">
+          <div className="relative w-full" ref={dropdownRef}>
+            {options && options.length > 0 ? (
+              <>
+                {input}
+                {showDropdown && (
+                  <div className="absolute z-10 mt-1 w-full max-h-40 overflow-y-auto bg-white border rounded-md shadow-lg custom-scrollbar">
+                    {filteredOptions.map((option, index) => (
+                      <div
+                        dir="rtl"
+                        key={index}
+                        className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                        onClick={() => selectOption(option)}
+                      >
+                        {option}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </>
+            ) : (
+              <>{input}</>
+            )}
+          </div>
+        </div>
+        <p className="m-2">{instruction}</p>
+      </div>
+      <label className="text-gray-500 text-sm font-semibold mb-2">
+        <span className="text-red-500">* </span>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default InputDropdown;
