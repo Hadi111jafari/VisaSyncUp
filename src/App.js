b351fd38ae7a9b9121092e67f7b4bea1de9f2d23
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2024-03-06 14:08:44 +0430

    Setup MUI date picker calender

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import WelcomePage from './components/WelcomePage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import HomePage from './pages/HomePage';
+import CheckVisaRequirements from './components/VisaApplication/CheckVisaRequirements';
+import FillVisaInformationForm from './components/VisaApplication/FillVisaInformationForm';
+import { LocalizationProvider } from '@mui/x-date-pickers/';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<WelcomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/request" element={<CheckVisaRequirements />} />
+          <Route path="/visa-form" element={<FillVisaInformationForm />} />
+        </Routes>
+      </Router>
+    </LocalizationProvider>
   );
 }
 
