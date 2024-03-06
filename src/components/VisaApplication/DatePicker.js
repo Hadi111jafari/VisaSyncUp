0a26682570c0ce8c003038cdc7e9ffbd0695f43d
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2024-03-06 14:09:15 +0430

    Build date picker component

---
+++ src/components/VisaApplication/DatePicker.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import dayjs from 'dayjs';
+
+const CustomDatePicker = ({ label }) => {
+  const [value, setValue] = useState(null);
+  const formattedDate = dayjs(value).format('YYYY-MM-DD');
+
+  return (
+    <div>
+      <DemoContainer components={['DatePicker']}>
+        <DatePicker
+          slotProps={{ textField: { size: 'small' } }}
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        />
+      </DemoContainer>
+      <label className="text-gray-500 text-sm font-semibold mb-2">
+        <span className="text-red-500">* </span>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default CustomDatePicker;
