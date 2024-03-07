431a4477c5893e6692f41f435ef49c47c84e073c
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2024-03-07 15:20:27 +0430

    Setup redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './state/store';
 import { I18nextProvider } from 'react-i18next';
 import App from './App';
 import i18n from './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <I18nextProvider i18n={i18n}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </I18nextProvider>
+  <Provider store={store}>
+    <I18nextProvider i18n={i18n}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </I18nextProvider>
+  </Provider>
 );
