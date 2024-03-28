952d2204badae669659a89213f3ceac29e12f7fa
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2024-03-28 16:21:46 +0430

    Fix warning related to purge and add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   // darkMode: false,
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        slideDown: 'pulse 0.5s ease-out',
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
