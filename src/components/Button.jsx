4bf10470960a78481d158b245d3f53b5830e827c
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2024-04-02 17:33:02 +0430

    Update file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { IoIosSave } from 'react-icons/io';
 
-const Button = ({ type, step, handleNext }) => {
+const Button = ({ type, handleNext }) => {
   return (
     <div >
       <button
         type={type}
         onClick={handleNext}
-        className={
-          step >= 2
-            ? 'flex sm:w-[197px] justify-center w-full bg-gray-100 hover:text-white text-green-500 font-bold border rounded-full py-2 px-4  hover:bg-green-600  transition duration-300 ease-in-out transform hover:scale-105'
-            : 'flex sm:w-[315px] w-full justify-center mt-2 bg-gray-100 hover:text-white text-green-500 font-bold border rounded-full py-2 px-4  hover:bg-green-600  transition duration-300 ease-in-out transform hover:scale-105'
-        }
+        className='flex sm:w-[315px] w-full justify-center mt-2 hover:text-white text-blue-600 font-bold border rounded-full py-2 px-4 hover:bg-blue-600  transition duration-300 ease-in-out transform hover:scale-105'
       >
         ثبت و ادامه
-        <span className="ml-2 mt-1.5">
+        <span className="ml-2 mt-0.5">
           <IoIosSave />
         </span>
       </button>
