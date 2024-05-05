f3b86b14154704e1fee0854b199d147aff659c20
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2024-05-05 13:30:33 +0430

    Fix deploy errors

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { useTranslation } from 'react-i18next';
 
 const Signup = () => {
   const [signupData, setSignupData] = useState({
